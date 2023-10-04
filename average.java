6ef97b37c8b8606686727985fcc8e3de9e96bcab
Michael Traynor <C21361681@mytudublin.ie>
2023-10-04 16:25:52 +0100

    progrsm to calculate the average

---
+++ average.java
@@ -0,0 +1,19 @@
+public class AverageCalculator {
+    public static void main(String[] args) {
+        // Create an array of numbers
+        double[] numbers = { 5.0, 7.0, 3.0, 2.0, 8.0, 9.0};
+
+        // Calculate the sum of numbers
+        double sum = 0.0;
+        for (double num : numbers) {
+            sum += num;
+        }
+
+        // Calculate the average
+        double average = sum / numbers.length;
+
+        // Print the average
+        System.out.println("Average: " + average);
+    }
+}
+
