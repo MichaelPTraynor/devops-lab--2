5f764bd08d970ad44ea4a17b451796b5e1745d7e
OwenJonesy <C21377551@mytudublin.ie>
2023-10-05 14:51:57 +0100

    YUP

--- average.java
+++ average.java
@@ -1,19 +1,33 @@
+
+
+
+import java.util.Scanner;
+
 public class AverageCalculator {
     public static void main(String[] args) {
-        // Create an array of numbers
-        double[] numbers = { 5.0, 7.0, 3.0, 2.0, 8.0, 9.0};
-
-        // Calculate the sum of numbers
-        double sum = 0.0;
-        for (double num : numbers) {
-            sum += num;
+        Scanner scanner = new Scanner(System.in);
+        
+        // Prompt the user to enter the number of values
+        System.out.print("Enter the number of values: ");
+        int numValues = scanner.nextInt();
+        
+        // Initialize variables for sum and average
+        double sum = 0;
+        
+        // Prompt the user to enter the values and calculate the sum
+        for (int i = 1; i <= numValues; i++) {
+            System.out.print("Enter value #" + i + ": ");
+            double value = scanner.nextDouble();
+            sum += value;
         }
-
+        
         // Calculate the average
-        double average = sum / numbers.length;
-
-        // Print the average
-        System.out.println("Average: " + average);
+        double average = sum / numValues;
+        
+        // Display the result
+        System.out.println("The average of the " + numValues + " values is: " + average);
+        
+        // Close the scanner
+        scanner.close();
     }
 }
-
